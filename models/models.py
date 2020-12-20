5b2fe0d4cd9261250505d0fa66d6b5613be0a0db
oscartomico <sinmail>
2020-12-20 16:38:56 +0100

    Añadido la búsqueda y filtrado y ejemplo de operaciones con el ORM

--- models/models.py
+++ models/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-
+import datetime
 
 class Visit(models.Model):
     _name = 'custom_crm.visit'
@@ -12,6 +12,33 @@ class Visit(models.Model):
     date = fields.Datetime(string='Fecha')
     type = fields.Selection([('P', 'Presencial'), ('W', 'WhatsApp'), ('T', 'Telefónico')], string='Tipo', required=True)
     done = fields.Boolean(string='Realizada', readonly=True)
+    image = fields.Binary(string='Imagen')
 
     def toggle_state(self):
         self.done = not self.done
+
+    def f_create(self):
+        visit = {
+            'name': 'ORM test',
+            'customer': 1,
+            'date': str(datetime.date(2020, 8, 6)),
+            'type': 'P',
+            'done': False
+        }
+        print(visit)
+        self.env['custom_crm.visit'].create(visit)
+
+    def f_search_update(self):
+        visit = self.env['custom_crm.visit'].search([('name', '=', 'ORM test')])
+        print('search()', visit, visit.name)
+
+        visit_b = self.env['custom_crm.visit'].browse([2])
+        print('browse()', visit_b, visit_b.name)
+
+        visit.write({
+            'name': 'ORM test write'
+        })
+
+    def f_delete(self):
+        visit = self.env['custom_crm.visit'].browse([2])
+        visit.unlink()
